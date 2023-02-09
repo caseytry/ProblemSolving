3e5c99223ed0bb3075962c023dfa5685d920d775
caseytry <casey.trytten@gmail.com>
2023-02-08 20:37:38 -0800

    Task class finished

--- chapt_3/printer_sim.py
+++ chapt_3/printer_sim.py
@@ -22,4 +22,15 @@ class Printer:
 
 
 class Task:
-    pass
+    def __init__(self, time):
+        self.timestamp = time
+        self.pages = random.randrange(1, 21)
+
+    def get_stamp(self):
+        return self.timestamp
+    
+    def get_pages(self):
+        return self.pages
+
+    def wait_time(self, current_time):
+        return current_time - self.timestamp
