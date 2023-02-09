fff6e655829a41e9dce605f87ab9b3c60a5f3364
caseytry <casey.trytten@gmail.com>
2023-02-08 19:44:47 -0800

    adding enqueue

--- chapt_3/Queue.py
+++ chapt_3/Queue.py
@@ -8,3 +8,7 @@ class Queue:
     def is_empty(self):
         """Check if the queue is empty"""
         return not bool(self._items)
+    
+    def enqueue(self, item):
+        """Add an item to the queue"""
+        self._items.insert(0, item)
