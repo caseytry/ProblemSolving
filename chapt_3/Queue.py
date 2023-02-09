c116b61d352dfd354bde4623da651839a01ace09
caseytry <casey.trytten@gmail.com>
2023-02-08 19:43:57 -0800

    adding is_empty

--- chapt_3/Queue.py
+++ chapt_3/Queue.py
@@ -5,4 +5,6 @@ class Queue:
         """Create new queue"""
         self._items = []
 
-    
+    def is_empty(self):
+        """Check if the queue is empty"""
+        return not bool(self._items)
