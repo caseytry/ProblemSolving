21da4a21cb5b82839111ab22d9898a8c56d5a7c9
caseytry <casey.trytten@gmail.com>
2023-02-08 20:47:42 -0800

    should be ready

--- chapt_3/printer_sim.py
+++ chapt_3/printer_sim.py
@@ -36,4 +36,34 @@ class Task:
         return current_time - self.timestamp
 
 def simulation(num_seconds,  pages_per_minute):
-    pass
+    lab_printer = Printer(pages_per_minute)
+    print_queue = Queue()
+    waiting_times = []
+
+    for current_second in range(num_seconds):
+        if new_print_task():
+            task = Task(current_second)
+            print_queue.enqueue(task)
+
+        if (not lab_printer.busy()) and (not print_queue.is_empty()):
+            nexttask = print_queue.dequeue()
+            waiting_times.append(nexttask.wait_time(current_second))
+            lab_printer.start_next(nexttask)
+
+        lab_printer.tick()
+    
+    average_wait = sum(waiting_times) / len(waiting_times)
+    print(
+        f"Average Wait {average_wait:6.2f} secs" \
+            +  f"{print_queue.size():3d} tasks remaining."
+    )
+
+
+def new_print_task():
+    num = random.randrange(1, 181)
+    return num == 180
+
+
+
+for i in range(10):
+    simulation(3600, 5)
