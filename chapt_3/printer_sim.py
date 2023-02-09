cd538a54b1f2f1fca7f60a8e634a2a065a692cf5
caseytry <casey.trytten@gmail.com>
2023-02-08 20:31:40 -0800

    tick method implemented

--- chapt_3/printer_sim.py
+++ chapt_3/printer_sim.py
@@ -5,3 +5,9 @@ class Printer:
         self.page_rate = ppm
         self.current_task = None
         self. time_remaining = 0
+
+    def tick(self):
+        if self.current_task is not None:
+            self.time_remaining = self.time_remaining - 1
+            if self.time_remaining <= 0:
+                self.current_task = None
